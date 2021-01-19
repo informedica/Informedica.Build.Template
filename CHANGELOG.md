160e8eafd3ef838647c4da395802c2a394c17043
Casper Bollen <halcwb@gmail.com>
2021-01-19 09:03:05 +0100

    chore: bump version to 1.1.0
    ## [1.1.0] - 2021-01-19
    
    Update to enable use for all Informedica projects
    
    ### Changed
    - Now also uses .net5
    - CI using Github actions
    - Contains Webserver fix for docs tool
    - Changed build script enabling FSI

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,15 +1,27 @@
 # Changelog
+
 All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [Unreleased]
+## [1.1.0] - 2021-01-19
+
+Update to enable use for all Informedica projects
+
+### Changed
+- Now also uses .net5
+- CI using Github actions
+- Contains Webserver fix for docs tool
+- Changed build script enabling FSI
 
 ## [0.1.0] - 2017-03-17
+
 First release
 
 ### Added
 - This release already has lots of features
 
-[0.1.0]: https://github.com/user/MyCoolNewLib.git/releases/tag/v0.1.0
+[Unreleased]: https://github.com/informedica/Informedica.Build.Template/compare/v1.1.0...HEAD
+[1.1.0]: https://github.com/informedica/Informedica.Build.Template/compare/v0.1.0...v1.1.0
+[0.1.0]: https://github.com/informedica/Informedica.Build.Template.git/releases/tag/v0.1.0
--- src/Informedica.Build.Template/AssemblyInfo.fs
+++ src/Informedica.Build.Template/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("Informedica.Build.Template")>]
 [<assembly: AssemblyProductAttribute("Informedica.Build.Template")>]
-[<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2017-03-17T00:00:00.0000000")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
+[<assembly: AssemblyVersionAttribute("1.1.0")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-01-19T00:00:00.0000000+01:00")>]
+[<assembly: AssemblyFileVersionAttribute("1.1.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("1.1.0")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","bb8964b54bee133e9af64d316dc2cfee16df7f72")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","958f9613daf38be3bca9b44b1b062887abd055e9")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Informedica.Build.Template"
     let [<Literal>] AssemblyProduct = "Informedica.Build.Template"
-    let [<Literal>] AssemblyVersion = "0.1.0"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2017-03-17T00:00:00.0000000"
-    let [<Literal>] AssemblyFileVersion = "0.1.0"
-    let [<Literal>] AssemblyInformationalVersion = "0.1.0"
+    let [<Literal>] AssemblyVersion = "1.1.0"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-01-19T00:00:00.0000000+01:00"
+    let [<Literal>] AssemblyFileVersion = "1.1.0"
+    let [<Literal>] AssemblyInformationalVersion = "1.1.0"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "bb8964b54bee133e9af64d316dc2cfee16df7f72"
+    let [<Literal>] AssemblyMetadata_GitHash = "958f9613daf38be3bca9b44b1b062887abd055e9"
